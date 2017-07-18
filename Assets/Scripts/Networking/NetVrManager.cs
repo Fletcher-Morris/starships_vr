c67f0934853f5cce2289b9a311537d1c8565b9ee
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-07-18 14:08:24 +0100

    Updates and fixes.

---
+++ Assets/Scripts/Networking/NetVrManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetVrManager : MonoBehaviour {
+
+    public GameObject myVrRig;
+
+    public GameObject myNetRig;
+
+    public GameObject myHead;
+    public GameObject myRightHand;
+    public GameObject myLeftHand;
+
+    private void Update()
+    {
+        if (!myVrRig)
+            myVrRig = GameObject.Find("[CameraRig]").gameObject;
+
+        if (!myHead)
+            myHead = myVrRig.transform.Find("Camera (eye)").gameObject;
+
+        if (!myRightHand)
+            myRightHand = myVrRig.transform.Find("Controller (right)").gameObject;
+
+        if (!myLeftHand)
+            myLeftHand = myVrRig.transform.Find("Controller (left)").gameObject;
+    }
+}
---
+++ Assets/Scripts/Networking/NetVrRig.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetVrRig : NetworkBehaviour {
+
+    public GameObject headObj;
+    public GameObject rightHandObj;
+    public GameObject leftHandObj;
+
+    public NetVrManager myMan;
+
+    public bool showHands = false;
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        myMan = GameObject.Find("NM").GetComponent<NetVrManager>();
+        myMan.myNetRig = gameObject;
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (myMan)
+        {
+            headObj.transform.position = myMan.myHead.transform.position * 2;
+            rightHandObj.transform.position = myMan.myRightHand.transform.position * 2;
+            leftHandObj.transform.position = myMan.myLeftHand.transform.position * 2;
+
+            headObj.transform.rotation = myMan.myHead.transform.rotation;
+            rightHandObj.transform.rotation = myMan.myRightHand.transform.rotation;
+            leftHandObj.transform.rotation = myMan.myLeftHand.transform.rotation;
+        }
+        else
+        {
+            GameObject.Destroy(gameObject);
+        }
+
+        if (!showHands)
+        {
+            headObj.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+            rightHandObj.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+            leftHandObj.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+        else
+        {
+            headObj.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = true;
+            rightHandObj.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = true;
+            leftHandObj.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
