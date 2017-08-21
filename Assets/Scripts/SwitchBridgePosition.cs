740390aaf994186fd0934f68a95a6822aec3ecfa
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-08-21 16:44:36 +0100

    Fixes

---
+++ Assets/Scripts/SwitchBridgePosition.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBridgePosition : MonoBehaviour
+{
+    public List<PositionRotation> positions;
+    public int currentPosition = 1;
+
+    private void Start()
+    {
+        SwitchPosition(1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(currentPosition + 1 <= positions.Count)
+            {
+                currentPosition++;
+                SwitchPosition(currentPosition);
+            }
+            else
+            {
+                currentPosition = 1;
+                SwitchPosition(currentPosition);
+            }
+        }
+    }
+
+    public void SwitchPosition(int posId)
+    {
+        transform.position = positions[posId - 1].position;
+        transform.eulerAngles = positions[posId - 1].rotation;
+    }
+}
+
+[System.Serializable]
+public class PositionRotation
+{
+    public Vector3 position;
+    public Vector3 rotation;
+}
