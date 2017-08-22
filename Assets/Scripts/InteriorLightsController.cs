a07ec533c5bff5d68b7b160ff25f79dca5596aad
THEBLACKBEAST\odmoa_000 <odmoandtheboy@gmail.com>
2017-08-22 16:59:31 +0100

    Interior lights, lightmaps, ui, and reflections now change colour when RED ALERT is activated or deactivated. Lighting improvements. Ship can now move forward and backward, but is pretty buggy.

---
+++ Assets/Scripts/InteriorLightsController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class InteriorLightsController : NetworkBehaviour
+{
+    public bool isRedAlert = false;
+
+    private LightmapData[] normalLightMaps;
+    private LightmapData[] redAlertLightMaps;
+    public Texture2D[] redAlertMapTex;
+
+    public List<GameObject> lights;
+    public Color redLightColour;
+    private Color normalLightColour;
+    public Material redLightMat;
+    public Material normalLightMat;
+
+    public List<GameObject> ribbonLights;
+    public Material redRibbonMat;
+    public Material blueRibbonMat;
+
+    public List<ReflectionProbe> reflectionProbes;
+
+    public List<GameObject> affectedUiItems;
+    public Color normalUiColour;
+    public Color redUiColour;
+
+    private void Start()
+    {
+        normalLightMaps = LightmapSettings.lightmaps;
+
+        redAlertLightMaps = new LightmapData[redAlertMapTex.Length];
+
+        for (int i = 0; i < redAlertMapTex.Length; i++)
+        {
+            redAlertLightMaps[i] = new LightmapData();
+            redAlertLightMaps[i].lightmapColor = redAlertMapTex[i];
+        }
+
+        normalLightColour = lights[0].GetComponent<Light>().color;
+    }
+
+    [Command]
+    public void CmdRedAlert()
+    {
+        RpcRedAlert();
+    }
+    [ClientRpc]
+    private void RpcRedAlert()
+    {
+        LightmapSettings.lightmaps = redAlertLightMaps;
+
+        foreach(GameObject light in lights)
+        {
+            light.GetComponent<Light>().color = redLightColour;
+            light.GetComponent<Renderer>().material = redLightMat;
+        }
+        foreach (GameObject light in ribbonLights)
+        {
+            light.GetComponent<Renderer>().material = redRibbonMat;
+        }
+        foreach(ReflectionProbe probe in reflectionProbes)
+        {
+            probe.RenderProbe();
+        }
+        foreach(GameObject ui in affectedUiItems)
+        {
+            if (ui.GetComponent<Image>())
+            {
+                ui.GetComponent<Image>().color = redUiColour;
+            }
+        }
+
+        isRedAlert = true;
+    }
+
+    [Command]
+    public void CmdNoAlert()
+    {
+        RpcNoAlert();
+    }
+    [ClientRpc]
+    private void RpcNoAlert()
+    {
+        LightmapSettings.lightmaps = normalLightMaps;
+
+        foreach (GameObject light in lights)
+        {
+            light.GetComponent<Light>().color = normalLightColour;
+            light.GetComponent<Renderer>().material = normalLightMat;
+        }
+        foreach (GameObject light in ribbonLights)
+        {
+            light.GetComponent<Renderer>().material = blueRibbonMat;
+        }
+        foreach (ReflectionProbe probe in reflectionProbes)
+        {
+            probe.RenderProbe();
+        }
+        foreach (GameObject ui in affectedUiItems)
+        {
+            if (ui.GetComponent<Image>())
+            {
+                ui.GetComponent<Image>().color = normalUiColour;
+            }
+        }
+
+        isRedAlert = false;
+    }
+
+    [Command]
+    public void CmdFlipAlert()
+    {
+        if (!isRedAlert)
+        {
+            RpcRedAlert();
+        }
+        else
+        {
+            RpcNoAlert();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            CmdFlipAlert();
+        }
+    }
+}
---
+++ Assets/Scripts/ShipMovementController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ShipMovementController : NetworkBehaviour
+{
+    public GameObject externalShip;
+    private Rigidbody shipRb;
+    public GameObject speedDisplayScreen;
+    public GameObject speedTargetScreen;
+
+    public int speedNotchSize = 100;
+
+    public int targetSpeed = 0;
+    public float currentSpeed = 0;
+    public float accelerationSpeed = 5000;
+    public int maxSpeed = 1000;
+
+    private void Start()
+    {
+        if (externalShip)
+        {
+            shipRb = externalShip.GetComponent<Rigidbody>();
+        }
+    }
+
+    [Command]
+    public void CmdSetDesiredSpeed(int speed)
+    {
+        speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+        RpcSetDesiredSpeed(speed);
+    }
+    [ClientRpc]
+    private void RpcSetDesiredSpeed(int speed)
+    {
+        targetSpeed = speed;
+    }
+    public void SpeedUp()
+    {
+        CmdSetDesiredSpeed(targetSpeed + speedNotchSize);
+    }
+    public void SlowDown()
+    {
+        CmdSetDesiredSpeed(targetSpeed - speedNotchSize);
+    }
+
+    private void Update()
+    {
+        AccellerateShip();
+        speedDisplayScreen.GetComponent<Text>().text = "SPEED: " + Mathf.RoundToInt(currentSpeed).ToString();
+        speedTargetScreen.GetComponent<Text>().text = targetSpeed.ToString();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SpeedUp();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SlowDown();
+        }
+    }
+
+    private void AccellerateShip()
+    {
+        if (targetSpeed > currentSpeed)
+        {
+            shipRb.AddRelativeForce(externalShip.transform.forward * -accelerationSpeed);
+        }
+        else if (targetSpeed < currentSpeed)
+        {
+            shipRb.AddRelativeForce(externalShip.transform.forward * accelerationSpeed);
+        }
+
+        shipRb.velocity = Vector3.ClampMagnitude(shipRb.velocity, maxSpeed);
+
+        currentSpeed = shipRb.velocity.magnitude;
+    }
+}
